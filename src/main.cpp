c7c3849b09a53c782483c41b91cc45cfc5f4412c
Pierre Ballif <pierre@ballif.eu>
2025-03-16 17:46:47 +0100

    integrate OpenGL

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,189 @@
 #include "grid.hpp"
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
 #include <iostream>
+#include <cstdlib>
+#include <stdexcept>
+#include <thread>
+#include <filesystem>
+#include <mutex>
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include <string_view>
+#include <cassert>
+
+// Copied from https://codereview.stackexchange.com/a/22907
+static std::vector<char> readAllBytes(std::filesystem::path filename) {
+    std::ifstream ifs(filename, std::ios::binary|std::ios::ate);
+    if(!ifs.is_open()){
+        std::stringstream error_msg;
+        error_msg << "File not found:" << filename;
+        throw std::runtime_error(error_msg.str());
+    }
+
+    std::ifstream::pos_type pos = ifs.tellg();
+
+    if (pos == 0) {
+        return std::vector<char>{};
+    }
+
+    std::vector<char> result(pos);
+
+    ifs.seekg(0, std::ios::beg);
+    ifs.read(&result[0], pos);
+
+    return result;
+}
+
+std::string_view get_error_msg(GLenum error){
+    switch(error){
+        case GL_NO_ERROR: return "No error";
+        case GL_INVALID_ENUM: return "GL_INVALID_ENUM";
+        case GL_INVALID_VALUE: return "GL_INVALID_VALUE";
+        case GL_INVALID_OPERATION: return "GL_INVALID_OPERATION";
+        case GL_INVALID_FRAMEBUFFER_OPERATION: return "GL_INVALID_FRAMEBUFFER_OPERATION";
+        case GL_OUT_OF_MEMORY: return "GL_OUT_OF_MEMORY";
+        case GL_STACK_UNDERFLOW: return "GL_STACK_UNDERFLOW";
+        case GL_STACK_OVERFLOW: return "GL_STACK_OVERFLOW";
+        default: return "Unrecognized";
+    }
+}
+
+struct Shader {
+    unsigned int shader;
+
+    Shader(std::filesystem::path filename, int shader_type, bool spirv = false) {
+        std::vector<char> program = readAllBytes(filename);
+
+        // Adapted from https://www.geeks3d.com/20200211/how-to-load-spir-v-shaders-in-opengl/
+        // See also https://www.khronos.org/opengl/wiki/SPIR-V#Example
+        assert(shader_type == GL_FRAGMENT_SHADER or shader_type == GL_VERTEX_SHADER);
+        shader = glCreateShader(shader_type);
+        if(shader == 0){
+            std::stringstream error_msg;
+            GLenum error = glGetError();
+            error_msg << "Could not create shader: " << get_error_msg(error) << " (" << error << ")";
+            throw std::runtime_error(error_msg.str());
+        }
+        if(spirv){
+            glShaderBinary(1, &shader, GL_SHADER_BINARY_FORMAT_SPIR_V_ARB, program.data(), program.size());
+            glSpecializeShader(shader, "main", 0, nullptr, nullptr);
+        } else {
+            const char* source[1] = { program.data() };
+            const GLint lengths[1] = { static_cast<int>(program.size()) };
+            glShaderSource(shader, 1, source, lengths);
+            glCompileShader(shader);
+        }
+
+        GLint compiled = 0;
+        glGetShaderiv(shader, GL_COMPILE_STATUS, &compiled);
+        if(compiled == GL_FALSE) {
+            GLint maxLength = 0;
+            glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &maxLength);
+
+            std::vector<GLchar> infoLog(maxLength);
+            glGetShaderInfoLog(shader, maxLength, &maxLength, infoLog.data());
+            std::stringstream error_msg;
+            error_msg << "Could not compile shader: " << std::string_view(infoLog.data(), infoLog.size()) << "(" << maxLength << " bytes log)";
+            throw std::runtime_error(error_msg.str());
+        }
+    }
+    ~Shader(){
+        glDeleteShader(shader);
+    }
+    operator int const () { return shader; }
+};
+
+unsigned int load_shader_program(std::filesystem::path vertex_shader, std::filesystem::path fragment_shader) {
+    unsigned int shader_program;
+    shader_program = glCreateProgram();
+    glAttachShader(shader_program, Shader(vertex_shader, GL_VERTEX_SHADER, true));
+    glAttachShader(shader_program, Shader(fragment_shader, GL_FRAGMENT_SHADER, true));
+    glLinkProgram(shader_program);
+    int success;
+    glGetProgramiv(shader_program, GL_LINK_STATUS, &success);
+    if(!success) {
+        throw std::runtime_error("Could not link program");
+    }
+    return shader_program;
+}
+
+class MyGLFW {
+    GLFWwindow* window;
+    std::thread ui_thread;
+    std::mutex mutex;
+    Grid* grid_to_render; // Protected by the mutex
+    bool should_exit; // Also protected by the mutex
+public:
+    MyGLFW() {
+        ui_thread = std::thread([this](){ this->thread_main(); });
+    }
+    ~MyGLFW() {
+        if(ui_thread.joinable()){
+            {
+                std::lock_guard guard(mutex);
+                should_exit = true;
+            }
+            ui_thread.join();
+        }
+        glfwDestroyWindow(window);
+        glfwTerminate();
+    }
+    void thread_main() {
+        if(!glfwInit()) {
+            throw std::runtime_error("Could not init GLFW");
+        }
+        glfwSetErrorCallback(this->error_callback);
+        window = glfwCreateWindow(640, 480, "Waves", nullptr, nullptr);
+        if(!window) {
+            throw std::runtime_error("Could not create window");
+        }
+        glfwMakeContextCurrent(window); // This needs to be before we initialize GLEW
+
+        GLenum err = glewInit();
+        if(err != GLEW_OK) {
+            std::stringstream err_msg;
+            err_msg << "Could not initialize GLEW:" << glewGetErrorString(err);
+            throw std::runtime_error(err_msg.str());
+        }
+        if(!GLEW_VERSION_2_1)
+            throw std::runtime_error("Bad GLEW version");
+        std::cerr << "Status: Using GLEW" << glewGetString(GLEW_VERSION) << '\n';
+        glDebugMessageCallback(this->debug_callback, nullptr);
+
+        int width, height;
+        glfwGetFramebufferSize(window, &width, &height);
+        glViewport(0, 0, width, height);
+
+        unsigned int shader_program = load_shader_program("res/vertex_shader.spv", "res/fragment_shader.spv");
+        glUseProgram(shader_program);
+
+        while(!glfwWindowShouldClose(window)) {
+            glfwPollEvents();
+            if(grid_to_render == nullptr) continue;
+            {
+                std::lock_guard guard(mutex);
+                if(grid_to_render != nullptr) continue;
+
+
+            }
+            glfwSwapBuffers(window);
+        }
+    }
+    static void error_callback(int error, const char* description){
+        std::cerr << "Error: " << description << '\n';
+    }
+    static void debug_callback(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar* message, const void* userParam) {
+        std::cerr << type << "," << id << "," << severity << "," << std::string_view(message, length) << '\n';
+    }
+    void render(const Grid& grid) {
+
+    }
+};
 
 int main() {
-    World world;
+    MyGLFW myGlfw;
 
-    for(int t = 0; t<50; t++) {
-        world.step();
-    }
-    for(const auto& row: world.grid()) {
-        for(const auto& cell: row) {
-            if(cell > 0) std::cout << cell;
-            else std::cout << "  ";
-        }
-        std::cout << std::endl;
-    }
+    World world;
 }
