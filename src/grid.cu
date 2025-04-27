c3173dde8b05d6e6eee7fcf26fb599d0e15a5085
Pierre Ballif <pierre@ballif.eu>
2025-04-27 11:01:40 +0200

    bug fix

--- src/grid.cu
+++ src/grid.cu
@@ -18,12 +18,12 @@ void cuda_step(PlainCGrid in, PlainCGrid out) {
     for(int i = 0; i < GRID_HEIGHT; i++) {
         out[i*GRID_WIDTH] = 1.0;
         for(int j = 1; j<GRID_WIDTH; j++) {
-            in[i*GRID_WIDTH + j] = out[i*GRID_WIDTH + j-1];
+            out[i*GRID_WIDTH + j] = in[i*GRID_WIDTH + j-1];
         }
     }
 }
 
 void World::step() {
-    cuda_step<<< 1, 1 >>>(grid1._data, grid2._data );
+    cuda_step<<< 1, 1 >>>(current_grid->_data, other_grid->_data );
     std::swap(other_grid, current_grid);
 }
