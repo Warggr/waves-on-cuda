265a7e2dfb43d531e427e68bb7bbab3fe8802d71
Pierre Ballif <pierre@ballif.eu>
2025-07-20 12:17:02 +0200

    feat: use multiple CUDA threads

--- src/grid.cu
+++ src/grid.cu
@@ -22,12 +22,33 @@ constexpr double SINE_FREQ = 2.0; // in 1 / time unit
 constexpr double SINE_FREQ_2PI = SINE_FREQ * 2 * M_PI;
 constexpr double WAVE_SPEED = 0.5; // in space unit / time unit.
 
-#ifndef NO_CUDA
-__global__
-#endif
-// C is the Courant number, c = v dx/dt. It must hold that 0 <= c < 1
-void cuda_step(const double* in, PlainCGrid out, double t, double c, std::size_t grid_width, std::size_t grid_height) {
+void step(
+    const double* in,
+    PlainCGrid out,
+    double t,
+    double c, // C is the Courant number, c = v dx/dt. It must hold that 0 <= c < 1
+    std::size_t grid_width, std::size_t grid_height
+) {
     for(int i = 0; i < grid_height; i++) {
+        out[i*grid_width] = sin(t * SINE_FREQ_2PI) + 1;
+        for(int j = 1; j<grid_width; j++) {
+            out[i*grid_width + j] =  in[i*grid_width + j] - c * (in[i*grid_width + j] - in[i*grid_width + j-1]);
+        }
+    }
+}
+
+__global__
+void cuda_step(
+    const double* in,
+    PlainCGrid out,
+    double t,
+    double c, // C is the Courant number, c = v dx/dt. It must hold that 0 <= c < 1
+    std::size_t grid_width, std::size_t grid_height,
+    std::size_t block_size
+) {
+    int start = threadIdx.x * block_size;
+    int end = (threadIdx.x + 1) * block_size;
+    for(int i = start; i < end; i++) {
         out[i*grid_width] = sin(t * SINE_FREQ_2PI) + 1;
         for(int j = 1; j<grid_width; j++) {
             out[i*grid_width + j] =  in[i*grid_width + j] - c * (in[i*grid_width + j] - in[i*grid_width + j-1]);
@@ -39,12 +60,12 @@ void World::step(bool sync) {
     const double c = WAVE_SPEED * grid1.cols() * dt;
     assert(c <= 1.0);
 #ifndef NO_CUDA
-    cuda_step<<< 1, 1 >>>(current_grid->_data, other_grid->_data, t, c, other_grid->rows(), other_grid->cols());
+    cuda_step<<< 1, other_grid->cols() >>>(current_grid->_data, other_grid->_data, t, c, other_grid->rows(), other_grid->cols(), 1);
     if (sync) {
         cudaDeviceSynchronize();
     }
 #else
-    cuda_step(current_grid->_data, other_grid->_data, t, c, other_grid->rows(), other_grid->cols());
+    step(current_grid->_data, other_grid->_data, t, c, other_grid->rows(), other_grid->cols());
 #endif
     std::swap(other_grid, current_grid);
     t += dt;
